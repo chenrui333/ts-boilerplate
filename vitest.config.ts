383649581a5397c71e95cb18c476461e71437908
Rui Chen <rui@chenrui.dev>
2025-05-31 13:48:31 -0400

    update vitest config
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
-
 export default defineConfig({
   test: {
     environment: 'node',
@@ -7,6 +7,9 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*.ts'],
+    },
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
     },
   },
 });
